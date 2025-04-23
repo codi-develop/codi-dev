96b12df9d1a18e29e0763fb8c107d2218ea52ef1
오현재 <55138387+hangooksaram@users.noreply.github.com>
2025-04-23 17:57:20 +0900

    style:lint "" -> ''

--- frontend/src/types/validate.ts
+++ frontend/src/types/validate.ts
@@ -1,10 +1,10 @@
-export type ValidateKeys = "required" | "regex" |"min" |"max"|"minLength";
-export interface ValidateInfo  {
-    message? : string;
-    value? : number | RegExp;
-}  
-export type ValidateSchemaValue  = Partial<Record<ValidateKeys, ValidateInfo>>
+export type ValidateKeys = 'required' | 'regex' | 'min' | 'max' | 'minLength';
+export interface ValidateInfo {
+  message?: string;
+  value?: number | RegExp;
+}
+export type ValidateSchemaValue = Partial<Record<ValidateKeys, ValidateInfo>>;
 
 export interface ValidateSchema {
-  [key:string]: ValidateSchemaValue
-  }
+  [key: string]: ValidateSchemaValue;
+}
